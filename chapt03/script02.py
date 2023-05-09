7f30e98a72d12c5e6db5a57b5ba0af49f7f66c14
Thiago Rodrigues <thiagoney@outlook.com>
2023-05-09 00:41:33 -0300

    Upload files on folder chapt03

---
+++ chapt03/script02.py
@@ -0,0 +1,91 @@
+"""
+Method Factory Pattern
+"""
+
+# Imports
+
+from abc import ABCMeta, abstractmethod
+
+
+# Class
+
+class Section(metaclass = ABCMeta):
+    """
+    Superclass
+    """
+    @abstractmethod
+    def describe(self):
+        pass
+
+class PersonalSection(Section):
+    """
+    Subclass
+    """
+    def describe(self):
+        print("Personal Section")
+
+class AlbumSection(Section):
+    """
+    Subclass
+    """
+    def describe(self):
+        print("Album Section")
+
+class PatentSection(Section):
+    """
+    Subclass
+    """
+    def describe(self):
+        print("Patent Section")
+
+class PublicationSection(Section):
+    """
+    Subclass
+    """
+    def describe(self):
+        print("Publication Section")
+
+class Profile(metaclass = ABCMeta):
+    """
+    Superclass
+    """
+    def __init__(self) -> None:
+        self.sections = []
+        self.create_profile()
+
+    @abstractmethod
+    def create_profile(self):
+        pass
+
+    def get_sections(self):
+        return self.sections
+
+    def add_sections(self, section):
+        self.sections.append(section)
+
+class Linkedin(Profile):
+    """
+    Subclass
+    """
+    def create_profile(self):
+        self.add_sections(PersonalSection())
+        self.add_sections(PatentSection())
+        self.add_sections(PublicationSection())
+
+class Facebook(Profile):
+    """
+    Subclass
+    """
+    def create_profile(self):
+        self.add_sections(PersonalSection())
+        self.add_sections(AlbumSection())
+
+
+# Main
+
+if __name__ == '__main__':
+    profile_type = input("Which Profile you'd like to create? [LinkedIn or Facebook]")
+
+    profile = eval(profile_type.title())()
+    print("Creating Profile..", type(profile).__name__)
+    print("Profile has sections --", profile.get_sections())
---
+++ chapt03/script03.py
@@ -0,0 +1,107 @@
+"""
+Abstract Factory Pattern
+"""
+
+# Imports
+
+from abc import ABCMeta, abstractmethod
+
+
+# Class
+
+class PizzaFactory(metaclass = ABCMeta):
+    """
+    Superclass
+    """
+    @abstractmethod
+    def create_veg_pizza(self):
+        pass
+
+    @abstractmethod
+    def create_non_veg_pizza(self):
+        pass
+
+class IndianPizzaFactory(PizzaFactory):
+    """
+    Subclass
+    """
+    def create_veg_pizza(self):
+        return DeluxVeggiePizza()
+
+    def create_non_veg_pizza(self):
+        return ChickenPizza()
+
+class USPizzaFactory(PizzaFactory):
+    """
+    Subclass
+    """
+    def create_veg_pizza(self):
+        return MexicanVegPizza()
+
+    def create_non_veg_pizza(self):
+        return HamPizza()
+
+class VegPizza(metaclass = ABCMeta):
+    """
+    Superclass
+    """
+    @abstractmethod
+    def prepare(self, VegPizza):
+        pass
+
+class NonVegPizza(metaclass = ABCMeta):
+    """
+    Superclass
+    """
+    @abstractmethod
+    def serve(self, VegPizza):
+        pass
+
+class DeluxVeggiePizza(VegPizza):
+    """
+    Subclass
+    """
+    def prepare(self):
+        print("Prepare ", type(self).__name__)
+
+class ChickenPizza(NonVegPizza):
+    """
+    Subclass
+    """
+    def serve(self, VegPizza):
+        print(type(self).__name__, " is served with Chicken on ",
+              type(VegPizza).__name__)
+
+class MexicanVegPizza(VegPizza):
+    """
+    Subclass
+    """
+    def prepare(self):
+        print("Prepare ", type(self).__name__)
+
+class HamPizza(NonVegPizza):
+    """
+    Subclass
+    """
+    def serve(self, VegPizza):
+        print(type(self).__name__, " is served with Ham on",
+              type(VegPizza).__name__)
+
+class PizzaStore:
+    def __init__(self) -> None:
+        pass
+
+    def make_pizzas(self):
+        for factory in [IndianPizzaFactory(), USPizzaFactory()]:
+            self.factory = factory
+            self.non_veg_pizza = self.factory.create_non_veg_pizza()
+            self.veg_pizza = self.factory.create_veg_pizza()
+            self.veg_pizza.prepare()
+            self.non_veg_pizza.serve(self.veg_pizza)
+
+
+# Main
+
+if __name__ == '__main__':
+    pizza = PizzaStore()
+    pizza.make_pizzas()
