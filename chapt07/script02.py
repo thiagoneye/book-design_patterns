0dbd5ad951a78f0e23a05582b6b9545ae4441e3f
Thiago Rodrigues <thiagoney@outlook.com>
2023-05-15 13:05:07 -0300

    Update files on chapt07

--- chapt07/script02.py
+++ chapt07/script02.py
@@ -4,15 +4,39 @@ Command Pattern
 
 # Imports
 
-
+from abc import ABCMeta, abstractmethod
 
 
 # Class
 
+class Command(metaclass=ABCMeta):
+    def __init__(self, recv):
+        self.recv = recv
 
+    def execute(self):
+        pass
+
+class ConcreteCommand(Command):
+    def execute(self):
+        self.recv.action()
+
+class Receiver:
+    def action(self):
+        print("Receiver Action")
+
+class Invoker:
+    def command(self, cmd):
+        self.cmd = cmd
+
+    def execute(self):
+        self.cmd.execute()
 
 
 # Main
 
 if __name__ == "__main__":
-    pass
+    recv = Receiver()
+    cmd = ConcreteCommand(recv)
+    invoker = Invoker()
+    invoker.command(cmd)
+    invoker.execute()
--- chapt07/script03.py
+++ chapt07/script03.py
@@ -4,15 +4,55 @@ Command Pattern
 
 # Imports
 
-
+from abc import ABCMeta, abstractmethod
 
 
 # Class
 
+class Order(metaclass=ABCMeta):
+    @abstractmethod
+    def execute(self):
+        pass
 
+class BuyStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+
+    def execute(self):
+        self.stock.buy()
+
+class SellStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+
+    def execute(self):
+        self.stock.sell()
+
+class StockTrade:
+    def buy(self):
+        print("You will buy stocks")
+
+    def sell(self):
+        print("You will sell stocks")
+
+class Agent:
+    def __init__(self):
+        self.__order_queue = []
+
+    def place_order(self, order):
+        self.__order_queue.append(order)
+        order.execute()
 
 
 # Main
 
 if __name__ == "__main__":
-    pass
+    # Client
+    stock = StockTrade()
+    buy_stock = BuyStockOrder(stock)
+    sell_stock = SellStockOrder(stock)
+
+    # Agent
+    agent = Agent()
+    agent.place_order(buy_stock)
+    agent.place_order(sell_stock)
